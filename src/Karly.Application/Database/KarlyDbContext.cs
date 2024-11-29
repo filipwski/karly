c65f10c9f9871cfdbdf6a0c7b7a3a0aea8ab573a
Filip Skibiński <filip.skibinski@schibsted.com>
2024-11-29 21:57:39 +0100

    feat: configure data seeding

--- src/Karly.Application/Database/KarlyDbContext.cs
+++ src/Karly.Application/Database/KarlyDbContext.cs
@@ -1,17 +1,40 @@
+using System.Text.Json;
 using Karly.Application.Database.EntityMapping;
 using Karly.Application.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Karly.Application.Database;
 
-public class KarlyDbContext(IConfiguration configuration) : DbContext
+public class KarlyDbContext(IConfiguration configuration, IHostEnvironment hostEnvironment) : DbContext
 {
     public DbSet<Car> Cars => Set<Car>();
-    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql(configuration.GetConnectionString("KarlyDbContext"));
+
+        if (hostEnvironment.IsDevelopment())
+        {
+            optionsBuilder.UseSeeding((context, _) =>
+            {
+                var car = context.Set<Car>().First();
+                if (car != null) return;
+                
+                var jsonPath = Path.Combine(
+                    Directory
+                        .GetParent(Directory.GetCurrentDirectory())
+                        .GetDirectories()
+                        .First(dir => dir.Name.Contains("Application")).FullName,
+                    "Database/Resources/ExampleCars.json");
+                var jsonString = File.ReadAllText(jsonPath);
+                var carList = JsonSerializer.Deserialize<List<Car>>(jsonString);
+
+                context.Set<Car>().AddRange(carList);
+                context.SaveChanges();
+            });
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
