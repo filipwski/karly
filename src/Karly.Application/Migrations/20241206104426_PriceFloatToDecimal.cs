906949cad343c057e6e81232c09ce8f9673a9b86
Filip Skibinski <filip.skibinski@gmail.com>
2024-12-06 11:51:45 +0100

    feat: add Price-related migrations

---
+++ src/Karly.Application/Migrations/20241206104426_PriceFloatToDecimal.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Karly.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class PriceFloatToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                type: "numeric(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Cars",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,2)");
+        }
+    }
+}
