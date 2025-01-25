57e9ae906fd910e8a8408f28234a173667f0fa3f
Filip Skibinski <filip.skibinski@gmail.com>
2025-01-25 13:11:04 +0100

    feat: install pgvector extension in docker compose

--- src/Karly.Application/Models/Car.cs
+++ src/Karly.Application/Models/Car.cs
@@ -2,10 +2,10 @@ namespace Karly.Application.Models;
 
 public class Car
 {
-    public required Guid Id { get; init; }
-    public required string Model { get; init; }
-    public required int ProductionYear { get; init; }
-    public required decimal Price { get; init; }
+    public Guid Id { get; init; }
+    public string Model { get; init; }
+    public int ProductionYear { get; init; }
+    public decimal Price { get; init; }
     public string Description { get; set; }
     public CarEmbedding? CarEmbedding { get; set; }
 }
