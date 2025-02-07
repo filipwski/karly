9aa81b1b9a26d313fc983dc150b1da06110f2d7a
Filip Skibinski <filip.skibinski@gmail.com>
2025-02-07 20:39:19 +0100

    feat: configure global Semantic Kernel

--- src/Karly.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/Karly.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,25 @@
+#pragma warning disable SKEXP0010
+
 using Karly.Api.Services;
+using Microsoft.SemanticKernel;
 
 namespace Karly.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddServices(this IServiceCollection services)
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<ICarService, CarService>();
+        services.AddOpenAiTextEmbeddingGeneration(configuration);
+    }
+
+    private static void AddOpenAiTextEmbeddingGeneration(this IServiceCollection services, IConfiguration configuration)
+    {
+        var openAiKey = configuration["Karly:OpenAiKey"];
+        if (openAiKey == null) throw new Exception("Karly:OpenAiKey is required");
+
+        services.AddOpenAITextEmbeddingGeneration("text-embedding-ada-002",
+            openAiKey);
+        services.AddScoped(serviceProvider => new Kernel(serviceProvider));
     }
 }
